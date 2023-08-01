ca1e2f7d6d76024bea72539f21cccebeff6ec9aa
Hirasawa Ishikawa <hirasawa_au@yahoo.co.jp>
2023-08-01 23:12:49 +0700

    Update doc

--- src/main.ts
+++ src/main.ts
@@ -14,8 +14,8 @@ const sessionModel = getModelForClass(SessionSchema);
 const tableModel = getModelForClass(TablesSchema);
 
 async function bootstrap() {
-  await mongoose.connect(process.env.MONGO_URI, {
-    dbName: process.env.MONGO_DBNAME,
+  await mongoose.connect(process.env.MONGO_URI ?? 'mongodb://localhost:27017', {
+    dbName: process.env.MONGO_DB_NAME ?? 'meeorder',
   });
 
   await addonModel.deleteMany({}).exec();
